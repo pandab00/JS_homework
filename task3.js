5785ff6de054c1afbec7ee2f1918c868295463d8
Dmitriy <dghoserr@gmail.com>
2024-01-17 16:59:42 +0300

    hw

--- task3.js
+++
@@ -1,5 +0,0 @@
-// Задание 3. Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
-
-const str = "Hello";
-const reversedString = str.split('').reverse().join('');
-console.log(reversedString);
--- task4.js
+++
@@ -1,4 +0,0 @@
-// Задание 4. Записать в переменную случайное целое число в диапазоне [0; 100]. Используйте объект Math.
-
-const randomNumber = Math.floor(Math.random() * 101);
-console.log(randomNumber);
