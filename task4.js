e23f11643bbb4aaaf693fe874b158959b232e183
Dmitriy <dghoserr@gmail.com>
2024-01-26 15:01:15 +0300

    final changes

--- task4.js
+++ task4.js
@@ -1,21 +1,77 @@
-// Задание 4.
+//Родительская функция
+function ElDevice(typeOfWork, power ) {
+  this.type = typeOfWork,
+  this.power = power,
+  this.isOn = false
+}
 
-// Напишите функцию, которая принимает два числа. Каждую секунду необходимо выводить в консоль, начиная от первого и заканчивая вторым. Используйте setInterval.
+ElDevice.prototype.switchOn = function() {
+  this.isOn = true;
+  console.log(`Device ${this.name} is active now.`)
+}
 
-// Например, пользователь ввёл числа 5 и 15. Каждую секунду в консоль должно печататься число, начиная с 5 и заканчивая 15 (всего 11 чисел: 5 6 7 8 9 10 11 12 13 14 15).
+ElDevice.prototype.switchOff = function() {
+  this.isOn = false;
+  console.log(`Device ${this.name} is off now.`)
+}
 
 
+//Создаем класс приборов - лампы.
+function Lamp(brand, brightness) {
+  this.name = brand,
+  this.brightness = brightness
+}
 
-function PrintNum(start, end) {
-    let begin = start;
-  
-    let interval = setInterval(function() {
-      console.log(begin);
-      if (begin === end) {
-        clearInterval(interval)
-      }
-      begin++;
-    }, 1000);
-  }
-  
-  PrintNum(5, 15)
+Lamp.prototype = new ElDevice('electro', 4);
+
+//Переопределяем классу лампы метод switchOn, чтобы он указывал дополнительно уровень яркости.
+Lamp.prototype.switchOn = function() {
+  this.isOn = true;
+  console.log(`Device ${this.name} is active now and it brightnes is ${this.brightness}`)
+}
+
+const lamp1 = new Lamp('filips', 'level 2');
+const lamp2 = new Lamp('sumsung', 'level High');
+console.log(lamp1);
+lamp1.switchOn();
+console.log(lamp2);
+lamp2.switchOn();
+
+
+//Создаем класс приборов - консоли.
+
+function GameConsole(brand, model, price) {
+  this.brand = brand,
+  this.model = model,
+  this.price = price
+}
+
+GameConsole.prototype = new ElDevice('electr', 7);
+
+
+//Создаем собственный метод для GameConsole
+
+GameConsole.prototype.getPrice = function() {
+  console.log(`Your price is ${this.price*0.8}`)
+}
+
+const ps5 = new GameConsole('Sony', 5, 800);
+const xBoxOne = new GameConsole('MSoft', 3, 600);
+
+console.log(ps5)
+
+ps5.getPrice()
+
+
+//суммируем мощность
+const sumPower = [];
+
+sumPower.push(ps5, xBoxOne, lamp1, lamp2);
+
+let totalPow = 0;
+
+for(let i = 0; i < sumPower.length; i++) {
+  totalPow += sumPower[i].power;
+}
+
+console.log("Суммарная потребляемая мощность всех приборов: " + totalPow + " Вт");
--- task5.js
+++ task5.js
@@ -1,13 +1,73 @@
-// Задание 5.
-
-// Напишите функцию, которая принимает два натуральных числа x и n и возвращает x в степени n. Иначе говоря, умножает x на себя n раз и возвращает результат.
-
-// Используйте Arrow Function синтаксис.
-
-// Протестируйте функцию на любых значениях и выведите результат в консоль.
-
-
-
-const getPow = (x, y) => x**y;
-
-console.log(getPow(3, 4));
+class ElDevice{
+    constructor(typeOfWork = 'electro') {
+      this.typeOfWork = typeOfWork;
+      this.isOn = false;
+    }
+    
+   switchOn() {
+     this.isOn = true;
+    console.log(`Device ${this.name} is active now.`)
+   }
+    
+    switchOff() {
+     this.isOn = false;
+    console.log(`Device ${this.name} is off now.`)
+   }
+    
+  }
+  
+  class Lamp extends ElDevice {
+    constructor(brand, power, brightness, typeOfWork) {
+      super(typeOfWork);
+      this.brand = brand;
+      this.power = power;
+      this.brightness = brightness;
+    }
+    
+    switchOn() {
+      this.isOn = true;
+      console.log(`Device "${this.brand}" is active now and it brightness is ${this.brightness}`)
+    }
+    
+  }
+  
+  const lamp1 = new Lamp('sumsung', 1.3, 'low');
+  const lamp2 = new Lamp('apple', 1.1, 'high');
+  
+  lamp1.switchOn()
+  
+  
+  class GameConsole extends ElDevice {
+    constructor(brand, power, model, typeOfWork) {
+      super(typeOfWork);
+      this.brand = brand;
+      this.power = power;
+      this.model = model;
+    }
+    
+    getModel() {
+      console.log( this.model +' - the last model of our product!')
+    }
+  }
+  
+  
+  const ps5 = new GameConsole('Sony', 2.3, 5);
+  const xBox = new GameConsole('MicroSoft', 2.2, 'One');
+  
+  xBox.getModel();
+  
+  //power
+  
+  const sumPower = [];
+  
+  sumPower.push(xBox, ps5, lamp1, lamp2);
+  
+  let totalPower = 0;
+  
+  for(let i = 0; i < sumPower.length; i++) {
+    totalPower += sumPower[i].power;
+  }
+  
+  function sumPow() {
+    console.log("Суммарная потребляемая мощность всех приборов: " + totalPower + " Вт");
+  }
