64389201e7e8c89997d53e0fa846f92a15ec3f1a
Dmitriy <dghoserr@gmail.com>
2024-02-06 16:16:14 +0300

    t1

--- task1.js
+++ task1.js
@@ -1,3 +1,34 @@
+// XML:
+
+// <list>
+//   <student>
+//     <name lang="en">
+//       <first>Ivan</first>
+//       <second>Ivanov</second>
+//     </name>
+//     <age>35</age>
+//     <prof>teacher</prof>
+//   </student>
+//   <student>
+//     <name lang="ru">
+//       <first>Петр</first>
+//       <second>Петров</second>
+//     </name>
+//     <age>58</age>
+//     <prof>driver</prof>
+//   </student>
+// </list>
+
+
+// JS-объект:
+
+// {
+//   list: [
+//     { name: 'Ivan Ivanov', age: 35, prof: 'teacher', lang: 'en' },
+//     { name: 'Петр Петров', age: 58, prof: 'driver', lang: 'ru' },
+//   ]
+// }
+
 const xmlString = `<list>
   <student>
     <name lang="en">
